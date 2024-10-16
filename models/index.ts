c5305ba94b8c42e7272eddd341e7c66a1d0a7f52
Aaron Delasy <aaron@thelang.io>
2024-10-16 16:11:47 +0300

    Convert Gemini model

--- models/index.ts
+++ models/index.ts
@@ -1,3 +1,4 @@
+import { gemini15pro } from "./gemini-1.5-pro";
 import { gpt4o } from "./gpt-4o";
 
 export interface ModelResult {
@@ -10,6 +11,9 @@ export async function runModel(
   map: string[][],
 ): Promise<ModelResult> {
   switch (modelId) {
+    case "gemini-1.5-pro": {
+      return gemini15pro(map);
+    }
     case "gpt-4o": {
       return gpt4o(map);
     }
