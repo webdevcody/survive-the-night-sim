087ec77268a6759f79b03a8a2132abcbbf0e21cf
Cody Seibert <webdevcody@gmail.com>
2024-10-16 17:27:54 -0400

    adding play delay

--- convex/constants.ts
+++ convex/constants.ts
@@ -13,6 +13,9 @@ export const AI_MODEL_IDS = AI_MODELS.map((model) => model.model);
 
 // how long between each level when the AI models start playing.
 // spacing out the levels to make it easier to watch in the games list and reduce ai token usage.
-export const PLAY_DELAY = process.env.PLAY_DELAY
-  ? parseInt(process.env.PLAY_DELAY)
-  : 10_000;
+export const PLAY_DELAY =
+  process.env.NODE_ENV === "development"
+    ? 0
+    : process.env.PLAY_DELAY
+      ? parseInt(process.env.PLAY_DELAY)
+      : 10_000;
