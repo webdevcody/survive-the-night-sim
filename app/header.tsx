4ec645e6ce46efbf1f479840fa35c12c99c7c0d4
Jaspal Singh <jaspalsinghgkp512@gmail.com>
2024-10-17 12:02:17 +0530

    add maps to db

--- app/header.tsx
+++ app/header.tsx
@@ -44,6 +44,9 @@ export default function Header() {
         <Link href="/leaderboard">
           <Button variant="ghost">Leaderboard</Button>
         </Link>
+        <Link href="/maps">
+          <Button variant="ghost">Add custom Maps</Button>
+        </Link>
         {flags?.showTestPage && (
           <Link href="/test">
             <Button variant="ghost">Test</Button>
