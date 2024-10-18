1efd2fd7cc342cc4cbb6ad0dcf8da94935dbc767
Cody Seibert <webdevcody@gmail.com>
2024-10-18 00:51:27 -0400

    update domains

--- app/provider.tsx
+++ app/provider.tsx
@@ -9,7 +9,11 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <PlausibleProvider domain="survive-the-night-sim-production.up.railway.app">
+    <PlausibleProvider
+      selfHosted
+      customDomain="https://plausible-analytics-ce-production-e655.up.railway.app"
+      domain="survive-the-night-sim-production.up.railway.app"
+    >
       <ThemeProvider attribute="class">
         <ConvexAuthNextjsProvider client={convex}>
           {children}
