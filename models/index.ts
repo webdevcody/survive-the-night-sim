cb87ddfd71120ee5487ee376a55671691e84b424
Aaron Delasy <aaron@thelang.io>
2024-10-18 20:09:21 +0300

    Fix convex compilation

--- models/index.ts
+++ models/index.ts
@@ -1,9 +1,9 @@
+import { ZombieSurvival } from "../simulators/zombie-survival";
 import { gemini15pro } from "./gemini-1.5-pro";
 import { gpt4o } from "./gpt-4o";
 import { claude35sonnet } from "./claude-3-5-sonnet";
 import { perplexityModel } from "./perplexity-llama";
 import { errorMessage } from "../lib/utils";
-import { ZombieSurvival } from "@/simulators/zombie-survival";
 
 export type ModelHandler = (
   prompt: string,
