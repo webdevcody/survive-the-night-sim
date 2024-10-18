452cb3d957ab97c4971751ee7c1830b9d518280d
Cody Seibert <webdevcody@gmail.com>
2024-10-18 00:01:48 -0400

    add plausible

--- app/provider.tsx
+++ app/provider.tsx
@@ -3,15 +3,21 @@
 import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ThemeProvider } from "next-themes";
+import PlausibleProvider from "next-plausible";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class">
-      <ConvexAuthNextjsProvider client={convex}>
-        {children}
-      </ConvexAuthNextjsProvider>
-    </ThemeProvider>
+    <PlausibleProvider
+      enabled={!!process.env.NEXT_PUBLIC_ENABLE_PLAUSIBLE}
+      domain="survive-the-night-sim-production.up.railway.app"
+    >
+      <ThemeProvider attribute="class">
+        <ConvexAuthNextjsProvider client={convex}>
+          {children}
+        </ConvexAuthNextjsProvider>
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
