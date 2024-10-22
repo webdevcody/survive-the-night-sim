ad6932907924eb94c7c6d38af35089dd6c39cabd
Cody Seibert <webdevcody@gmail.com>
2024-10-22 11:41:36 -0400

    push fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,21 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <ConvexAuthNextjsServerProvider>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(s,r,t){
-              s._umamiQueue=s._umamiQueue||[];
-              s._umamiConfig=s._umamiConfig||{};
-              s._umamiConfig.websiteId="6e6265bb-25ed-4f8b-819b-44a27434ed51";
-              s._umamiConfig.src="https://umami-production-101d.up.railway.app/script.js";
-              var a=r.createElement(t),m=r.getElementsByTagName(t)[0];
-              a.async=1;a.src=s._umamiConfig.src;
-              m.parentNode.insertBefore(a,m);
-            })(window,document,"script");
-          `,
-        }}
-      />
+      <script defer src="https://umami-production-101d.up.railway.app/script.js"
+        data-website-id="6e6265bb-25ed-4f8b-819b-44a27434ed51"
+      ></script>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <Providers>
