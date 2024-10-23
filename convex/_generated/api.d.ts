42d73b54e95640e8d8cf4f105e715b51ef891942
Jakob Rössner <jakob.roessner@outlook.de>
2024-10-24 01:56:56 +1100

    revert #d5b08673

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,3 +1,5 @@
+/* prettier-ignore-start */
+
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -64,3 +66,5 @@ export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
+
+/* prettier-ignore-end */
