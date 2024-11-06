cfa66a90ebdccbbb8e2c69e4d400ee47c2e33e0d
Cody Seibert <webdevcody@gmail.com>
2024-11-06 15:01:38 -0500

    revert umami changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/umami.js",
-        destination: "http://145.223.79.119:3000/script.js",
-      },
-      {
-        source: "/api/send",
-        destination: "http://145.223.79.119:3000/api/send",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
